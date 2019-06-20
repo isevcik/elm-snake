1f868adb8a45263c4eb6e50957131e4687b896dc
Ivan Sevcik <iv.sevcik@gmail.com>
2019-06-20 23:53:08 +0200

    final game functionality done

--- src/Main.elm
+++ src/Main.elm
@@ -6,7 +6,8 @@ import Svg.Attributes exposing (..)
 import Time
 import Json.Decode as Decode
 import Random
-import List.Extra exposing (..)
+import List.Extra
+import Maybe exposing (..)
 
 main =
     Browser.element
@@ -16,19 +17,19 @@ main =
         , view = view
         }
 
+tickInterval = 50
+playgroundSize = Coords 600 600
+elementSize = 15
+initialHead = Coords (playgroundSize.x // 2) (playgroundSize.y // 2)
+initialLength = 5
+initialBody = List.indexedMap (\i coords -> Coords coords.x (coords.y + i)) (List.repeat initialLength initialHead)
+initialSnake = Snake initialHead initialBody Down Down
+initialModel = Model PressKeyToStart initialSnake (Coords 0 0 ) 0
 
 type alias Coords =
     { x : Int
     , y : Int
     }
-
-tickInterval = 50
-playgroundSize = Coords 600 600
-elementSize = 20
-initialHead = Coords (playgroundSize.x // 2) (playgroundSize.y // 2)
-initialLength = 5
-initialBody = List.indexedMap (\i coords -> Coords coords.x (coords.y + i)) (List.repeat initialLength initialHead)
-initialSnake = Snake initialHead initialBody Down Down
 
 type Status
     = PressKeyToStart
@@ -52,28 +53,38 @@ type alias Model =
 type Msg
     = Tick Time.Posix
     | KeyDown Direction
+    | KeyPressed String
     | NewFoodPosition Coords
+
+type Direction
+  = Up
+  | Right
+  | Down
+  | Left
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch [
-        Browser.Events.onKeyDown (Decode.map KeyDown keyDecoder)
+        Browser.Events.onKeyDown (Decode.map KeyPressed keyDecoder)
         , Time.every tickInterval Tick
     ]
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model PressKeyToStart initialSnake (Coords 0 0 ) 0 , generateFoodPosition initialSnake )
+    ( initialModel, generateFoodPosition initialSnake )
 
+initInGame : () -> ( Model, Cmd Msg )
+initInGame _ =
+    ( { initialModel | state = InGame }, generateFoodPosition initialSnake )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of 
+    case msg of
         Tick  _ ->
             case model.state of
                 InGame ->
                     let
-                        snake = resolveRequestedDirection model.snake 
+                        snake = resolveRequestedDirection model.snake
                         nextPosition = getNextPosition snake
                         scoring = isScoring model nextPosition
                         collision = List.member nextPosition snake.body
@@ -87,9 +98,24 @@ update msg model =
 
                 _ ->
                     ( model, Cmd.none )
-            
+        KeyPressed key ->
+            case key of
+                " " ->
+                    case model.state of
+                        PressKeyToStart ->
+                            initInGame ()
+                        GameOver ->
+                            initInGame ()
+                        _ ->
+                            ( model, Cmd.none )
+                _ ->
+                    let
+                        maybeDirection = keyToDirection key
+                        snakeBefore = model.snake
+                    in
+                    ( { model | snake = { snakeBefore | requestedDirection = withDefault model.snake.requestedDirection maybeDirection } }, Cmd.none )
         KeyDown direction ->
-            let 
+            let
                 snakeBefore = model.snake
             in
             ( { model | snake = { snakeBefore | requestedDirection = Debug.log "direction" direction } }, Cmd.none )
@@ -112,7 +138,7 @@ isDirectionValid snake direction =
             ( Left, Right )
             ]
     in
-    not <| List.member ( snake.direction, direction) invalidCombinations 
+    not <| List.member ( snake.direction, direction) invalidCombinations
         || List.member ( direction, snake.direction) invalidCombinations
 
 resolveRequestedDirection : Snake -> Snake
@@ -121,7 +147,6 @@ resolveRequestedDirection snake =
         direction = if isDirectionValid snake snake.requestedDirection then snake.requestedDirection else snake.direction
     in
     { snake | direction = direction }
-    
 
 moveSnake : Coords -> Snake -> Bool -> Snake
 moveSnake headPosition snake grow =
@@ -130,7 +155,6 @@ moveSnake headPosition snake grow =
         newBody = snake.head :: List.take (bodyLength - if grow then 0 else 1) snake.body
     in
     { snake | head = headPosition, body = newBody }
-
 
 snakeCoords : Snake -> List Coords
 snakeCoords snake =
@@ -145,44 +169,55 @@ popSnake snake =
     { snake | body = if List.length snake.body > 5 then List.take 5 snake.body else snake.body }
 
 unshiftSnake : Coords -> Snake -> Snake
-unshiftSnake coords snake = 
+unshiftSnake coords snake =
     { snake | body = coords :: snake.body }
 
 view : Model -> Html Msg
 view model =
     svg [ width <| String.fromInt <| playgroundSize.x, height <| String.fromInt <| playgroundSize.y
         , viewBox <| String.join " " <| List.map String.fromInt [0, 0, playgroundSize.x, playgroundSize.y] ]
-        <| List.concat [ 
+        <| List.concat [
             [ rect [ x "0", y "0", width "100%", height "100%", fill "black" ] [] ]
             ,
          case model.state of
             PressKeyToStart ->
-                [ svgText "50" "50" "Press a key to start" ]
-
+                svgPressToStart
             InGame ->
-                text_ [ x "5", y "15", Svg.Attributes.style "fill: white" ] 
-                      [ Svg.text ( "Score: " ++ ( String.fromInt model.score ) ) ]
-                ::svgText "5" "30" ( "head: " ++ (String.fromInt model.snake.head.x) ++ ", " ++ (String.fromInt model.snake.head.y))
-                ::svgText "5" "45" ( "food: " ++ (String.fromInt model.foodPosition.x) ++ ", " ++ (String.fromInt model.foodPosition.y))
+                svgScore model.score
+
                 ::rect [ x (String.fromInt model.foodPosition.x), y (String.fromInt model.foodPosition.y), width <| svgIntToPixels elementSize, height <| svgIntToPixels elementSize, rx "0.3%", fill "red" ] []
                 ::svgSnake model.snake
-                
+
 
             GameOver ->
-                text_ [ x "10", y "10", Svg.Attributes.style "fill: white" ] [ Svg.text "Game over" ]
-                ::svgSnake model.snake
+                svgGameOver model.score
+                ++ svgPressToStart
+                ++ svgSnake model.snake
         ]
+
+svgDebugInfo model =
+    Svg.g [] [ svgText 5 30 ( "head: " ++ (String.fromInt model.snake.head.x) ++ ", " ++ (String.fromInt model.snake.head.y)) []
+          ,svgText 5 45 ( "food: " ++ (String.fromInt model.foodPosition.x) ++ ", " ++ (String.fromInt model.foodPosition.y)) []
+    ]
+
+svgScore score =
+    svgText 20 28 ("SCORE: " ++ String.fromInt score) [ Svg.Attributes.style "fill: white; font-family: monospace; font-size: 16px;" ]
+
+svgPressToStart =
+    [ svgText ( playgroundSize.x // 2 ) ( playgroundSize.y // 2 ) "Press SPACE to start" [ Svg.Attributes.style "fill: white; font-size: 20px; font-family: monospace; text-anchor: middle" ] ]
+
+svgGameOver score =
+    [ svgText ( playgroundSize.x // 2 ) ( playgroundSize.y // 2 - 30 ) ( "Game Over, Your Score: " ++ String.fromInt score ) [ Svg.Attributes.style "fill: white; font-size: 20px; font-family: monospace; text-anchor: middle" ] ]
 
 svgSnake snake =
     List.map svgSnakeElement ( snake.head :: snake.body )
 
-
-svgText x y text =
-    text_ [ Svg.Attributes.x x, Svg.Attributes.y y, Svg.Attributes.style "fill: white" ] [ Svg.text text ]
-
--- playground : Model -> Html Msg
--- playground model =
---     List.map snakeElement model
+svgText : Int -> Int -> String -> List (Svg.Attribute msg) -> Svg msg
+svgText x y text attrs =
+    let
+        attributes = [ Svg.Attributes.x <| String.fromInt <| x, Svg.Attributes.y <| String.fromInt <| y, Svg.Attributes.style "fill: white" ] ++ attrs
+    in
+    text_ attributes [ Svg.text text ]
 
 svgSnakeElement : Coords -> Svg Msg
 svgSnakeElement coords =
@@ -219,7 +254,7 @@ allPlaygroundCoords =
         yRange = List.range 0 (playgroundSize.y // elementSize - 1)
         yCoors = List.map (\y -> y * elementSize) yRange
     in
-        List.concatMap (\x -> List.map ( Coords x ) yCoors) xCoors 
+        List.concatMap (\x -> List.map ( Coords x ) yCoors) xCoors
 
 generateFoodPosition : Snake -> Cmd Msg
 generateFoodPosition snake =
@@ -229,34 +264,24 @@ generateFoodPosition snake =
         justOrOrigin = (\n -> case n of
             Just a -> a
             Nothing -> Coords 0 0)
-        coordsGenerator = Random.map (\n -> justOrOrigin (getAt n allCoordsWithoutSnake)) (Random.int 0 max)
+        coordsGenerator = Random.map (\n -> justOrOrigin (List.Extra.getAt n allCoordsWithoutSnake)) (Random.int 0 max)
     in
         Random.generate NewFoodPosition coordsGenerator
 
-    
--- Keyboard
-
-type Direction
-  = Up
-  | Right
-  | Down
-  | Left
-
-keyDecoder : Decode.Decoder Direction
+keyDecoder : Decode.Decoder String
 keyDecoder =
   Decode.field "key" Decode.string
-    |> Decode.andThen directionDecoder
 
-directionDecoder : String -> Decode.Decoder Direction
-directionDecoder string =
+keyToDirection : String -> Maybe Direction
+keyToDirection string =
   case string of
     "ArrowUp" ->
-        Decode.succeed Up
+        Just Up
     "ArrowRight" ->
-        Decode.succeed Right
+        Just Right
     "ArrowDown" ->
-        Decode.succeed Down
+        Just Down
     "ArrowLeft" ->
-        Decode.succeed Left
+        Just Left
     _ ->
-        Decode.fail "not an arrow key"
+        Nothing
